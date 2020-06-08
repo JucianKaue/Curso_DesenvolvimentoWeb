888bce71e28b14f7119e520037a47b2f818aa075
Jucian Decezare <jucian_decezare2015@hotmail.com>
2020-06-08 14:15:03 -0300

    aula 14

---
+++ JavaScript/exercicios/aula14/ambiente.js
@@ -0,0 +1,12 @@
+/*
+var c = 1 CARIAVEL DE CONTROLE
+while (c <= 5) -> TESTE LÓGICO {
+    console.log(c)
+    c++ -> INCREMENTO
+}
+*/
+//-------------------
+console.log('Vai começar... ')
+for (var c = 1; c<=5; c++) {
+    console.log(c)
+}
